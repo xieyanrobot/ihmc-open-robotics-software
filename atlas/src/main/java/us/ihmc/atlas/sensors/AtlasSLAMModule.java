e927d5b34a939eb1f27916cbb3aedee7cf5f0712
edile <edile@DESKTOP-1RF6LR7.attlocal.net>
2020-06-05 23:29:11 -0500

    Added footstepstatus handler and api for footstep data.

--- atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSLAMModule.java
+++ atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSLAMModule.java
@@ -3,24 +3,29 @@ package us.ihmc.atlas.sensors;
 import java.io.File;
 import java.io.IOException;
 
+import controller_msgs.msg.dds.FootstepDataMessage;
+import controller_msgs.msg.dds.FootstepStatusMessage;
 import controller_msgs.msg.dds.RobotConfigurationData;
 import controller_msgs.msg.dds.StampedPosePacket;
 import us.ihmc.atlas.parameters.AtlasSensorInformation;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ControllerAPIDefinition;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.util.NetworkPorts;
 import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
+import us.ihmc.humanoidRobotics.communication.packets.walking.FootstepStatus;
 import us.ihmc.messager.Messager;
 import us.ihmc.pubsub.subscriber.Subscriber;
 import us.ihmc.robotEnvironmentAwareness.communication.KryoMessager;
 import us.ihmc.robotEnvironmentAwareness.communication.REACommunicationProperties;
 import us.ihmc.robotEnvironmentAwareness.communication.SLAMModuleAPI;
 import us.ihmc.robotEnvironmentAwareness.slam.SLAMModule;
+import us.ihmc.robotics.robotSide.RobotSide;
 
 public class AtlasSLAMModule extends SLAMModule
 {
-   private void updateStationaryStatus(Subscriber<RobotConfigurationData> subscriber)
+   private void handleRobotConfigurationData(Subscriber<RobotConfigurationData> subscriber)
    {
       RobotConfigurationData robotConfigurationData = subscriber.takeNextData();
       latestRobotTimeStamp.set(robotConfigurationData.getMonotonicTime());
@@ -44,6 +49,23 @@ public class AtlasSLAMModule extends SLAMModule
       }
    }
 
+   private void handleFootstepStatusMessage(Subscriber<FootstepStatusMessage> subscriber)
+   {
+      FootstepStatusMessage footstepStatusMessage = subscriber.takeNextData();
+      if (footstepStatusMessage.getFootstepStatus() == FootstepStatus.COMPLETED.toByte())
+      {
+         RobotSide robotSide = RobotSide.fromByte(footstepStatusMessage.getRobotSide());
+         Point3DReadOnly footLocation = footstepStatusMessage.getActualFootPositionInWorld();
+         QuaternionReadOnly footOrientation = footstepStatusMessage.getActualFootOrientationInWorld();
+
+         FootstepDataMessage footstepDataMessageToSubmit = new FootstepDataMessage();
+         footstepDataMessageToSubmit.setRobotSide(robotSide.toByte());
+         footstepDataMessageToSubmit.getLocation().set(footLocation);
+         footstepDataMessageToSubmit.getOrientation().set(footOrientation);
+         reaMessager.submitMessage(SLAMModuleAPI.FootstepDataState, footstepDataMessageToSubmit);
+      }
+   }
+
    public AtlasSLAMModule(Messager messager, DRCRobotModel drcRobotModel, File configurationFile)
    {
       super(messager, configurationFile);
@@ -51,7 +73,12 @@ public class AtlasSLAMModule extends SLAMModule
       ROS2Tools.createCallbackSubscriptionTypeNamed(ros2Node,
                                                     RobotConfigurationData.class,
                                                     ROS2Tools.getControllerOutputTopic(drcRobotModel.getSimpleRobotName()),
-                                                    this::updateStationaryStatus);
+                                                    this::handleRobotConfigurationData);
+
+      ROS2Tools.createCallbackSubscriptionTypeNamed(ros2Node,
+                                                    FootstepStatusMessage.class,
+                                                    ROS2Tools.getControllerOutputTopic(drcRobotModel.getSimpleRobotName()),
+                                                    this::handleFootstepStatusMessage);
 
       estimatedPelvisPublisher = ROS2Tools.createPublisherTypeNamed(ros2Node,
                                                                     StampedPosePacket.class,
